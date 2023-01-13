50f3a9f4a90210c7190b432efe45db13f9aff74a
Simon <sschol24@smail.uni-koeln.de>
2023-01-13 12:40:28 +0100

    Add CrossOrigin

--- bookstore/src/main/java/de/uni/koeln/se/bookstore/controller/BookController.java
+++ bookstore/src/main/java/de/uni/koeln/se/bookstore/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import de.uni.koeln.se.bookstore.service.BookService;
 import de.uni.koeln.se.bookstore.datamodel.Book;
 
 @RequestMapping("/bookstore")
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class BookController {
 	@Autowired
